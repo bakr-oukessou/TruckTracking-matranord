936b31b7eb89a98dc996e3ee4bd52c4703cd790c
bakr oukessou <bakr.oukessou@gmail.com>
2024-06-29 16:37:08 +0100

    code style config

--- Matranord-tracking-BackEnd/Matranord-tracking-BackEnd/src/main/java/com/matranord/Matranord_tracking_BackEnd/MatranordTrackingBackEndApplication.java
+++ Matranord-tracking-BackEnd/Matranord-tracking-BackEnd/src/main/java/com/matranord/Matranord_tracking_BackEnd/MatranordTrackingBackEndApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MatranordTrackingBackEndApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MatranordTrackingBackEndApplication.class, args);
 	}
